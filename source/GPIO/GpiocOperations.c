99864d11a98c9f9fa4ab5a676468aaa50894d694
Terry Biberdorf <tbiberdorf@littelfuse.com>
2019-12-13 15:18:36 -0700

    add gpio support to set options and display settings

--- source/GPIO/GpiocOperations.c
+++ source/GPIO/GpiocOperations.c
@@ -32,6 +32,13 @@
 #define BOARD_SW_NAME BOARD_SW1_NAME
 
 static uint32_t tl_ButtonPressed = false;
+static uint32_t tl_ChangeConfigurationFlag = 0;
+
+typedef enum _PinSelection_e
+{
+	ePinSelectedNone,
+	ePinSelectedOne,
+}ePinSelection_t;
 
 typedef enum _PullUpPower_e
 {
@@ -53,13 +60,14 @@ typedef enum _ActiveFilter_e
 
 typedef struct _InputPinOperation_s
 {
-	uint32_t PinSelection;
+	ePinSelection_t PinSelection;
 	ePullUpPower_t PullUpPower;
 	ePassiveFilter_t PassiveFilter;
 	ePassiveFilter_t ActiveFilter;
 }sInputPinOperation_t;
 
 sInputPinOperation_t tl_CurrentInputPin = {
+		.PinSelection = ePinSelectedOne,
 		.PullUpPower=ePullUpPowerHigh,
 		.PassiveFilter=ePassiveFilterOn,
 		.ActiveFilter=eActiveFilterOff};
@@ -94,15 +102,177 @@ void PORTC_IRQHandler(void)
 uint32_t setInputPin(uint32_t Pin)
 {
 
-	return Pin;
+	tl_CurrentInputPin.PinSelection = ePinSelectedOne;
+	tl_ChangeConfigurationFlag = 1;
+	return tl_CurrentInputPin.PinSelection = ePinSelectedOne;
 }
 
+/**
+ * @brief modify the Pullup power selection
+ */
+uint32_t setPullupPowerLevel(uint32_t PowerLevel)
+{
+	uint32_t powerLevel = 0;
+	if(PowerLevel)
+	{
+		tl_CurrentInputPin.PullUpPower = ePullUpPowerHigh;
+		powerLevel = 1;
+	}
+	else
+	{
+		tl_CurrentInputPin.PullUpPower = ePullUpPowerLow;
+	}
+
+	tl_ChangeConfigurationFlag = 1;
+	return powerLevel;
+}
+
+/**
+ * @brief modify the passive filter selection
+ */
+uint32_t setPassiveFilter(uint32_t PassiveFilter)
+{
+	uint32_t passiveFilter= 0;
+	if(PassiveFilter)
+	{
+		tl_CurrentInputPin.PassiveFilter = ePassiveFilterOn;
+		passiveFilter = 1;
+	}
+	else
+	{
+		tl_CurrentInputPin.PassiveFilter = ePassiveFilterOff;
+	}
+
+	tl_ChangeConfigurationFlag = 1;
+	return passiveFilter;
+}
+
+/**
+ * @brief modify the active filter selection
+ */
+uint32_t setActiveFilter(uint32_t ActiveFilter)
+{
+	uint32_t activeFilter= 0;
+	if(ActiveFilter)
+	{
+		tl_CurrentInputPin.ActiveFilter = eActiveFilterOn;
+		ActiveFilter = 1;
+	}
+	else
+	{
+		tl_CurrentInputPin.ActiveFilter = eActiveFilterOff;
+	}
+
+	tl_ChangeConfigurationFlag = 1;
+	return ActiveFilter;
+}
+
+/**
+ * @brief display current input pin selection settings
+ */
 void getInputPinConfiguration(char *CurrentConfigurationPtr,size_t *Size)
 {
+	const char *pinConfigStr1 ="Input Pin: ";
+	const char *pinConfigStr2 ="one\r\n";
+	const char *pinConfigStr3 ="none\r\n";
+	const char *pinConfigStr4 ="Pullup Pwr: ";
+	const char *pinConfigStr5 ="high\r\n";
+	const char *pinConfigStr6 ="low\r\n";
+	const char *pinConfigStr7 ="Passive Filter: ";
+	const char *pinConfigStr8 ="on\r\n";
+	const char *pinConfigStr9 ="off\r\n";
+	const char *pinConfigStr10 ="Active Filter: ";
+	const char *pinConfigStr11 ="on\r\n";
+	const char *pinConfigStr12 ="off\r\n";
+
 	size_t sentSize = 0;
+	size_t length;
 	char *ptrDetails = CurrentConfigurationPtr;
-	sentSize = strlen("Input Pin: ");
-	memcpy(ptrDetails,"Input Pin: ",sentSize);
+	length = strlen(pinConfigStr1);
+	memcpy(ptrDetails,pinConfigStr1,length);
+	ptrDetails += length;
+	sentSize += length;
+	switch (tl_CurrentInputPin.PinSelection)
+	{
+	case ePinSelectedOne:
+		length = strlen(pinConfigStr2);
+		memcpy(ptrDetails,pinConfigStr2,length);
+		ptrDetails += length;
+		sentSize += length;
+		break;
+	case ePinSelectedNone:
+		length = strlen(pinConfigStr3);
+		memcpy(ptrDetails,pinConfigStr3,length);
+		ptrDetails += length;
+		sentSize += length;
+		break;
+	}
+
+
+	// display pullup strength
+	length = strlen(pinConfigStr4);
+	memcpy(ptrDetails,pinConfigStr4,length);
+	ptrDetails += length;
+	sentSize += length;
+	switch (tl_CurrentInputPin.PullUpPower)
+	{
+	case ePullUpPowerHigh:
+		length = strlen(pinConfigStr5);
+		memcpy(ptrDetails,pinConfigStr5,length);
+		ptrDetails += length;
+		sentSize += length;
+		break;
+	case ePullUpPowerLow:
+		length = strlen(pinConfigStr6);
+		memcpy(ptrDetails,pinConfigStr6,length);
+		ptrDetails += length;
+		sentSize += length;
+		break;
+	}
+
+	// display passive filter settings
+	length = strlen(pinConfigStr7);
+	memcpy(ptrDetails,pinConfigStr7,length);
+	ptrDetails += length;
+	sentSize += length;
+	switch (tl_CurrentInputPin.PassiveFilter)
+	{
+	case ePassiveFilterOn:
+		length = strlen(pinConfigStr8);
+		memcpy(ptrDetails,pinConfigStr8,length);
+		ptrDetails += length;
+		sentSize += length;
+		break;
+	case ePassiveFilterOff:
+		length = strlen(pinConfigStr9);
+		memcpy(ptrDetails,pinConfigStr9,length);
+		ptrDetails += length;
+		sentSize += length;
+		break;
+	}
+
+
+	// display Active filter settings
+	length = strlen(pinConfigStr10);
+	memcpy(ptrDetails,pinConfigStr10,length);
+	ptrDetails += length;
+	sentSize += length;
+	switch (tl_CurrentInputPin.ActiveFilter)
+	{
+	case eActiveFilterOn:
+		length = strlen(pinConfigStr11);
+		memcpy(ptrDetails,pinConfigStr11,length);
+		ptrDetails += length;
+		sentSize += length;
+		break;
+	case eActiveFilterOff:
+		length = strlen(pinConfigStr12);
+		memcpy(ptrDetails,pinConfigStr12,length);
+		ptrDetails += length;
+		sentSize += length;
+		break;
+	}
+
 	*Size = sentSize;
 }
 
@@ -112,6 +282,11 @@ void GpioTask(void *pvParameters)
 
 	while(1)
 	{
+		if(tl_ChangeConfigurationFlag)
+		{
+			tl_ChangeConfigurationFlag = 0;
+			printf("change gpio input configuration\r\n");
+		}
 		vTaskDelay(1000);
 	}
 }
